e927dd78c3dfabd378177f2a19fdc460ecb8eeed
Isaac Bookstaff <ibookstaff@outlook.com>
2023-10-31 00:11:41 -0500

    Added user programs to mmap.c and munmap.c to help with debugging, also changed the CPU count in the makefile to 1

---
+++ xv6-public/mmap.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fs.h"
+
+
+int
+main(int argc, char *argv[])
+{
+  printf(1, "This is the mmap system call, printed in mmap.c\n");
+  exit();
+}
