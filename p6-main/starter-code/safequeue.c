0228c9f6e46a14f82e84908767276d7b7fc9e983
BRAEDEN BERTZ <bbertz@vm-instunix-10.cs.wisc.edu>
2023-11-14 17:19:01 -0600

    added header file outline

---
+++ p6-main/starter-code/safequeue.c
@@ -0,0 +1,21 @@
+#include "safequeue.h"
+
+void create_queue()
+{
+}
+
+void add_work()
+{
+}
+
+struct http_request get_work()
+{
+    struct http_request h;
+    return h;
+}
+
+struct http_request get_work_nonblocking()
+{
+    struct http_request h;
+    return h;
+}
